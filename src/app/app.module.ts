08a9f8cae53a72a8aa3ff52d7700e056f6cb6928
Prodan <proboy599@gmail.com>
2020-12-09 14:29:17 +0100

    creando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './components/login/login.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { TaskCreationComponent } from './components/task-creation/task-creation.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    TaskListComponent,
+    TaskCreationComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
