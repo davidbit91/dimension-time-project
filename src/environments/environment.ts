578976a9d469d444d51320799c5348a0154d934d
davidbit91 <david.garcia@student.bit.es>
2020-12-10 11:03:01 +0100

    dp

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,14 @@
 
 export const environment = {
   production: false,
-
+  firebaseConfig: {
+    apiKey: 'AIzaSyBqnvwilziRpPamUtlPXkcqkqTrDAKPRuE',
+    authDomain: 'dimensional-time.firebaseapp.com',
+    projectId: 'dimensional-time',
+    storageBucket: 'dimensional-time.appspot.com',
+    messagingSenderId: '65221145565',
+    appId: '1:65221145565:web:e4ba56bd1450fedc707c34',
+  },
 };
 
 /*
