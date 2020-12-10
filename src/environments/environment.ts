48ac47de3e473cd6877d71fd8e8b9bc0e87c68ae
Prodan <proboy599@gmail.com>
2020-12-10 11:00:35 +0100

    lol

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBqnvwilziRpPamUtlPXkcqkqTrDAKPRuE',
+    authDomain: 'dimensional-time.firebaseapp.com',
+    projectId: 'dimensional-time',
+    storageBucket: 'dimensional-time.appspot.com',
+    messagingSenderId: '65221145565',
+    appId: '1:65221145565:web:e4ba56bd1450fedc707c34',
+  },
 };
 
 /*
